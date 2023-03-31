e845cdd555710d399961ea526821bd6f912786b8
cl2493 <102442157+cl2493@users.noreply.github.com>
2023-03-31 09:53:23 -0700

    Add files via upload
    Uploaded my code and the Pokemon file.

---
+++ cs205_final_exam.sh
@@ -0,0 +1,38 @@
+# TODO: Modify this file to create a shell script that is able to use awk to go through a file
+ # formatted like pokemon.dat and provides a printed report in the following format (where your
+ # script correctly calculates the values that go into the [VALUE] placeholders):
+ # ======= SUMMARY OF POKEMON.DAT ======
+ #    Total Non-Legendary Pokemon: [VALUE]
+ #    Avg. HP: [VALUE]
+ #    Avg. Defense: [VALUE]
+ # ======= END SUMMARY =======
+
+ # NOTE THAT YOU MUST USE AWK OR YOU WILL LOSE POINTS
+ # The "Avg." values should be calculated as mean values for the corresponding columns.
+ # The averages must only be for non-legendary pokemon.
+ # The spacing and header formatting should match the above formatting description exactly.
+ # There should be a comment explaining the purpose of each line in your shell script.
+ # The data file will be passed in to the script as a positional parameter and will not
+ # necessarily be called pokemon.dat. However, you can assume that any file passed to this
+ # script will be formatted exactly the way pokemon.dat is formatted.
+
+ awk BEGIN{FS="\t"}
+ {
+     if (NR!=1){
+
+     totalPoke+=1
+
+     averageHP+=$5
+
+     averageAttack+=$6
+     }
+     }
+     END{
+
+     echo " ======= SUMMARY OF POKEMON.DAT ======"
+     printf "    Total Pokemon: " totalPoke
+     printf "    Avg. HP: " averageHP/totalPoke
+     printf "    Avg. Attack: " averageAttack/totalPoke
+     echo " ======= END SUMMARY ======="
+
+ } pokemon.dat
